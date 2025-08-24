01245c96dd56d618c5726d370d681c5afa74adb6
ShamilShigapov <sshigapov1990@gmail.com>
2025-08-24 16:26:34 +0300

    Практическая работа по WinForms ДЗ от 22.08.2025

---
+++ Lesson_5_1_Graphic/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Lesson_5_1_Graphic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb_ShowPicture = new System.Windows.Forms.PictureBox();
+            this.dgv_ShowData = new System.Windows.Forms.DataGridView();
+            this.bt_ShowAll = new System.Windows.Forms.Button();
+            this.tb_InsertID = new System.Windows.Forms.TextBox();
+            this.bt_ShowOne = new System.Windows.Forms.Button();
+            this.bt_LoadPicture = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ShowPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ShowData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_ShowPicture
+            // 
+            this.pb_ShowPicture.Location = new System.Drawing.Point(397, 98);
+            this.pb_ShowPicture.Name = "pb_ShowPicture";
+            this.pb_ShowPicture.Size = new System.Drawing.Size(391, 321);
+            this.pb_ShowPicture.TabIndex = 11;
+            this.pb_ShowPicture.TabStop = false;
+            // 
+            // dgv_ShowData
+            // 
+            this.dgv_ShowData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_ShowData.Location = new System.Drawing.Point(12, 98);
+            this.dgv_ShowData.Name = "dgv_ShowData";
+            this.dgv_ShowData.Size = new System.Drawing.Size(379, 321);
+            this.dgv_ShowData.TabIndex = 10;
+            // 
+            // bt_ShowAll
+            // 
+            this.bt_ShowAll.Location = new System.Drawing.Point(530, 31);
+            this.bt_ShowAll.Name = "bt_ShowAll";
+            this.bt_ShowAll.Size = new System.Drawing.Size(150, 40);
+            this.bt_ShowAll.TabIndex = 9;
+            this.bt_ShowAll.Text = "Show all";
+            this.bt_ShowAll.UseVisualStyleBackColor = true;
+            this.bt_ShowAll.Click += new System.EventHandler(this.bt_ShowAll_Click);
+            // 
+            // tb_InsertID
+            // 
+            this.tb_InsertID.Location = new System.Drawing.Point(357, 42);
+            this.tb_InsertID.Name = "tb_InsertID";
+            this.tb_InsertID.Size = new System.Drawing.Size(150, 20);
+            this.tb_InsertID.TabIndex = 8;
+            // 
+            // bt_ShowOne
+            // 
+            this.bt_ShowOne.Location = new System.Drawing.Point(188, 31);
+            this.bt_ShowOne.Name = "bt_ShowOne";
+            this.bt_ShowOne.Size = new System.Drawing.Size(150, 40);
+            this.bt_ShowOne.TabIndex = 7;
+            this.bt_ShowOne.Text = "Show one";
+            this.bt_ShowOne.UseVisualStyleBackColor = true;
+            this.bt_ShowOne.Click += new System.EventHandler(this.bt_ShowOne_Click);
+            // 
+            // bt_LoadPicture
+            // 
+            this.bt_LoadPicture.Location = new System.Drawing.Point(22, 31);
+            this.bt_LoadPicture.Name = "bt_LoadPicture";
+            this.bt_LoadPicture.Size = new System.Drawing.Size(150, 40);
+            this.bt_LoadPicture.TabIndex = 6;
+            this.bt_LoadPicture.Text = "Load Picture";
+            this.bt_LoadPicture.UseVisualStyleBackColor = true;
+            this.bt_LoadPicture.Click += new System.EventHandler(this.bt_LoadPicture_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pb_ShowPicture);
+            this.Controls.Add(this.dgv_ShowData);
+            this.Controls.Add(this.bt_ShowAll);
+            this.Controls.Add(this.tb_InsertID);
+            this.Controls.Add(this.bt_ShowOne);
+            this.Controls.Add(this.bt_LoadPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ShowPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ShowData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_ShowPicture;
+        private System.Windows.Forms.DataGridView dgv_ShowData;
+        private System.Windows.Forms.Button bt_ShowAll;
+        private System.Windows.Forms.TextBox tb_InsertID;
+        private System.Windows.Forms.Button bt_ShowOne;
+        private System.Windows.Forms.Button bt_LoadPicture;
+    }
+}
+
---
+++ Lesson_5_1_Graphic/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson_5_1_Graphic
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = null;
+        SqlDataAdapter dataqAdapter = null;
+        DataSet dataSet = null;
+        string str = "";
+        string fileName = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            str = ConfigurationManager.ConnectionStrings["Company_db"].ConnectionString;
+            conn = new SqlConnection(str);
+        }
+
+        private void bt_LoadPicture_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Графические файлы | *bmp; *.png; *.jpeg; *.gif";
+            ofd.FileName = "";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                fileName = ofd.FileName;
+                LoadPicture();
+            }
+        }
+
+        private void LoadPicture()
+        {
+            try
+            {
+                byte[] bytes = CreateCopy();
+
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("insert into dbo.Pictures(Customer_ID, _Name, Picture)" +
+                                                "values (@customerID, @name, @picture);", conn);
+
+                if (tb_InsertID.Text == null || tb_InsertID.Text.Length == 0)
+                {
+                    return;
+                }
+
+                int index = -1;
+                int.TryParse(tb_InsertID.Text, out index);
+
+                cmd.Parameters.Add("@customerID", SqlDbType.Int).Value= index;
+                cmd.Parameters.Add("@name", SqlDbType.NVarChar, 255).Value = fileName;
+                cmd.Parameters.Add("@picture", SqlDbType.Image, bytes.Length).Value = bytes;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error LoadPicture");
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private byte[] CreateCopy()
+        {
+            try
+            {
+                Image img = Image.FromFile(fileName);
+
+                int maxWidth = 300, maxHeight = 300;
+                double ratioX = (double)maxWidth / img.Width;
+                double ratioY = (double)maxHeight / img.Height;
+                double ratio = Math.Min(ratioX, ratioY);
+                int newWidth = (int)(img.Width * ratio);
+                int newHeight = (int)(img.Height * ratio);
+
+                Image currentImage = new Bitmap(newWidth, newHeight);
+                Graphics g = Graphics.FromImage(currentImage);
+                g.DrawImage(currentImage, 0, 0, newWidth, newHeight);
+
+                MemoryStream ms = new MemoryStream();
+                currentImage.Save(ms, ImageFormat.Jpeg);
+
+                ms.Flush();
+                ms.Seek(0, SeekOrigin.Begin);
+
+                BinaryReader br = new BinaryReader(ms);
+                byte[] buf = br.ReadBytes((int)ms.Length);
+
+                return buf;
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error CreateCopy");
+
+                return null;
+            }
+        }
+
+        private void bt_ShowAll_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+                dataqAdapter = new SqlDataAdapter("select * from dbo.Pictures;", conn);
+
+                SqlCommandBuilder cmd = new SqlCommandBuilder(dataqAdapter);
+
+                dataSet=new DataSet();
+
+                dataqAdapter.Fill(dataSet, "picture");
+
+                dgv_ShowData.DataSource = dataSet.Tables["Picture"];
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void bt_ShowOne_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (tb_InsertID.Text == null || tb_InsertID.Text.Length == 0)
+                {
+                    MessageBox.Show("Укажите id клиента");
+                    return;
+                }
+                int index = -1;
+                int.TryParse(tb_InsertID.Text, out index);
+                if (index == -1)
+                {
+                    MessageBox.Show("Укажите id клиента  в правильном формате");
+                    return;
+                }
+                dataqAdapter = new SqlDataAdapter("select Picture from dbo.Pictures where Customer_ID=@Id", conn);
+                SqlCommandBuilder cmb = new SqlCommandBuilder(dataqAdapter);
+                dataqAdapter.SelectCommand.Parameters.Add("@Id", SqlDbType.Int).Value = index;
+                dataSet = new DataSet();
+                dataqAdapter.Fill(dataSet);
+                byte[] bytes = (byte[])dataSet.Tables[0].Rows[0]["Picture"];
+                MemoryStream ms = new MemoryStream(bytes);
+                pb_ShowPicture.Image = Image.FromStream(ms);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
